c348b43e89b74ff81fbafc8b0f80133d24f335ab
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-08-25 13:37:30 +0100

    fixed error for task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # manifest that kills a process named killmenow
-exec { 'killmenow':
-  command  => 'pkill -x -f killmenow',
-  onlyif   => 'pgrep -x -f killmenow',
+exec { 'killmenow_process':
+  command  => 'pkill killmenow',
+  onlyif   => 'pgrep killmenow',
   provider => 'shell'
 }
