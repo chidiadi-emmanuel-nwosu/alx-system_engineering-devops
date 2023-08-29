5301dae2e1fabb0dcbb42114aa56acaded61d041
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-08-29 10:39:52 +0100

    refactored code for advance task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -16,8 +16,7 @@ exec { 'create_return_string':
 exec { 'configure_server':
   provider => shell,
   command  => @(END)
-    echo '
-    server {
+    printf %s "server {
     	listen 80;
     	listen [::]:80 default_server;
     	root   /etc/nginx/html;
@@ -26,7 +25,7 @@ exec { 'configure_server':
     	location /redirect_me {
             	return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
     	}
-    }' | sudo tee /etc/nginx/sites-available/default
+    }" | sudo tee /etc/nginx/sites-available/default
   END
 }
 
