8ebea5dcda36433df5a8083ec4d0641fe2695d8f
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-10-11 22:02:54 +0100

    added comment in puppet file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
-exec { 'fix-wordpress-server':
+# puppet file to fix apache server
+exec { 'fix-apache-server':
   provider => 'shell',
   command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
 }
